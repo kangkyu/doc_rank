9e85e0297e24d9693d082d96d75ce85fcc3eac04
“s1v4” <hdao61@gmail.com>
2017-09-05 19:56:41 -0700

    The beginnings of the doc_rank gem

---
+++ lib/doc_rank.rb
@@ -0,0 +1,5 @@
+require "doc_rank/version"
+
+module DocRank
+  # Your code goes here...
+end
---
+++ spec/doc_rank_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe DocRank do
+  it "has a version number" do
+    expect(DocRank::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
