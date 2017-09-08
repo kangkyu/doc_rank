dc01fbf69360bac6d558985b5ee5711dd981b01c
“s1v4” <hdao61@gmail.com>
2017-09-07 17:38:12 -0700

    Travis and Coverall integration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
 require "bundler/setup"
 require "doc_rank"
 
