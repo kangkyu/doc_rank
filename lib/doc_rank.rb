928f0ad6e8cc6ef07f45cdf991cb826eeb0f1e16
Sergey Ganous <sergeyganous@gmail.com>
2017-09-12 23:44:15 -0700

    Added weight_compare Method
    * Added additional method to compare the key values of documents and rank them by weight.

--- lib/doc_rank.rb
+++ lib/doc_rank.rb
@@ -14,4 +14,21 @@ module DocRank
       [doc.name, matches.size]
     end.sort_by{|doc| doc[1]}.to_h
   end
+
+  def self.weight_compare(base, targets)
+    base_keywords = DocRank::Document.new(base).keywords.to_a
+    Array(targets).map do |target|
+      grand_sum = 0
+      doc = DocRank::Document.new target
+      doc_keywords = doc.keywords.to_a
+      base_keywords.each do |base_keyword|
+        doc_keywords.each do |doc_keyword|
+          if base_keyword.text == doc_keyword.text
+            grand_sum += (base_keyword.weight + doc_keyword.weight)
+          end
+        end
+      end
+      [doc.name, grand_sum]
+    end.sort_by{|doc| doc[1]}.to_h
+  end
 end
