7451db8a82d19518b81289265513c27548baa579
“s1v4” <hdao61@gmail.com>
2017-09-14 10:00:10 -0700

    removed duplicate code between score and weighted_score

--- lib/doc_rank/ranker.rb
+++ lib/doc_rank/ranker.rb
@@ -22,30 +22,30 @@ module DocRank
     #   document regardless of weight.
     # @return [Hash] a hash of document names mapped to their matches.
     def scores
-      @score ||= begin
-        results = @target_docs.map do |target|
-          matches = @base_kws.keys & target.weighted_keywords.keys
-          [target.name, matches.count]
-        end
-        rank(results)
-      end
+      @score ||= rank match_keywords
     end
 
     # Rank the target documents based on any keyword matches with the base
     #   document by summing the weights of any match.
     # @return [Hash] a hash of document names mapped to their scores.
     def weighted_scores
-      @weighted_scores ||= begin
-        results = @target_docs.map do |target|
-          target_kws = target.weighted_keywords
-          matches = @base_kws.keys & target_kws.keys
-          [target.name, matches.sum {|match| @base_kws[match] + target_kws[match]}]
-        end
-        rank(results)
-      end
+      @weighted_scores ||= rank match_keywords(weighted: true)
     end
 
   private
+
+    def match_keywords(options = {})
+      @target_docs.map do |target|
+        target_kws = target.weighted_keywords
+        matches = @base_kws.keys & target_kws.keys
+        score = options[:weighted] ? sum_weights(matches, target_kws) : matches.count
+        [target.name, score]
+      end
+    end
+
+    def sum_weights(matches, target)
+      matches.sum {|match| @base_kws[match] + target[match]}
+    end
 
     def rank(docs)
       docs.sort_by{|doc| doc[1]}.to_h
