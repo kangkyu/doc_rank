c8d095ac960cec4af54ceda648ec7ad3c2e17172
Sergey Ganous <sergeyganous@gmail.com>
2017-09-14 22:42:37 -0700

    Added Unit Tests for Ranker Class
    * Added unit tests for functions in the DocRank::Ranker class.

---
+++ spec/ranker/scores_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+RSpec.describe 'DocRank::Ranker#scores' do
+  let(:base) { "#{RSPEC_ROOT}/sample_texts/job_desc.txt" }
+  let(:target) {[
+    "#{RSPEC_ROOT}/sample_texts/resume1.doc",
+  ]}
+  let(:mult_targets) {[
+    "#{RSPEC_ROOT}/sample_texts/resume2.docx",
+    "#{RSPEC_ROOT}/sample_texts/resume3.pdf"
+  ]}
+
+  it 'returns a hash table of name and score of the target document.' do
+    ranker = DocRank::Ranker.new base, target
+    expect(ranker.scores).to eq({"resume1.doc"=>36})
+  end
+
+  it 'returns a hash table of name and score of multiple target documents.' do
+    ranker = DocRank::Ranker.new base, mult_targets
+    expect(ranker.scores).to eq({"resume2.docx"=>11, "resume3.pdf"=>32})
+  end
+end
